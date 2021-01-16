3560b5169f5471009233e0353fee49339c7410d1
Foxandxss <foxandxss@gmail.com>
2021-01-16 20:02:45 +0100

    typeorm configuration

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -16,6 +17,18 @@ import * as Joi from '@hapi/joi';
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
       }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      // logging:
+      //   process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
+      entities: [],
     }),
   ],
 })
